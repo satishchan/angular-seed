dae6c87900aea6eb7ce04880980dab3c9f188e91
satishchan <satish.chandrasekara@rackspace.com>
2014-08-03 17:37:34 -0400

    update with filter and route - WIP

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,9 +4,27 @@ angular.module('F1FeederApp.controllers', []).
     $scope.driversList = [];
     $scope.seasonYear = null;
 
+    $scope.searchFilter = function (driver) {
+        var re = new RegExp($scope.nameFilter, 'i');
+        return !$scope.nameFilter || re.test(driver.Driver.givenName) || re.test(driver.Driver.familyName);
+    };
+    
     ergastAPIservice.getDrivers().success(function (response) {
         //Dig into the responde to get the relevant data
         $scope.seasonYear = response.MRData.StandingsTable.StandingsLists[0].season;
         $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
     });
+    /* Driver controller */
+  controller('driverController', function($scope, $routeParams, ergastAPIservice) {
+    $scope.id = $routeParams.id;
+    $scope.races = [];
+    $scope.driver = null;
+
+    ergastAPIservice.getDriverDetails($scope.id).success(function (response) {
+        $scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]; 
+    });
+
+    ergastAPIservice.getDriverRaces($scope.id).success(function (response) {
+        $scope.races = response.MRData.RaceTable.Races; 
+    }); 
   });
